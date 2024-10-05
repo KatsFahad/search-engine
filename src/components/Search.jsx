7846c337650f4951b3dc068bc8e23356914ccfb3
katemba fahad <katsfahad008@gmail.com>
2024-10-05 11:17:08 +0300

    Add search function to filter results

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,22 +1,39 @@
 import { useEffect, useState } from "react";
 
 const Search = () => {
-  const [users, setUsers] = useState(null);
+  const [users, setUsers] = useState([]);
+
+  const [filterUsers, setfilterUsers] = useState([]);
 
   const fetchUsers = () => {
     const apiUrl = "https://task-manager-back-end-2.onrender.com/users";
 
     fetch(apiUrl)
-    .then((response)=>{
-        return response.json()
-    })
-    .then((data)=>{
-        setUsers(data)
-    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setUsers(data);
+      });
   };
-  useEffect(()=>{
-    fetchUsers()
-  },[])
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const handleFilter = (value) => {
+    const searchValue = value.toLowerCase();
+
+    if (searchValue === "") {
+      setfilterUsers([]);
+      return;
+    }
+    const result = users.filter(
+      (u) =>
+        u.name.toLowerCase().includes(searchValue) ||
+        u.email.toLowerCase().includes(searchValue)
+    );
+    setfilterUsers(result);
+  };
   return (
     <div>
       <h2>Search Engine</h2>
@@ -24,17 +41,24 @@ const Search = () => {
 
       <div>
         <div>
-          <input type="text" placeholder="Search here" />
+          <input
+            type="text"
+            placeholder="Search here"
+            onChange={(e) => handleFilter(e.target.value)}
+          />
         </div>
         <div>
-            {
-                users !== null ? (
-                    users.map((user, index)=>{
-                        return <h4 key={index}>{user.name} <br /> {user.email}</h4>
-                    })
-                ):
-                <p>No users</p>
-            }
+          {filterUsers.length > 0 ? (
+            filterUsers.map((user, index) => {
+              return (
+                <h4 key={index}>
+                  {user.name} <br /> {user.email}
+                </h4>
+              );
+            })
+          ) : (
+            <p>No users</p>
+          )}
         </div>
       </div>
     </div>

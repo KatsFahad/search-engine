ed2c0f85a4bbf81ee8f1de3a06083cd789c1d739
katemba fahad <katsfahad008@gmail.com>
2024-10-05 10:05:44 +0300

    Add search component

---
+++ src/components/Search.jsx
@@ -0,0 +1,16 @@
+
+const Search = () => {
+  return (
+    <div>
+        <h2>Search Engine</h2>
+        <p>Search a User by name or email</p>
+
+        <div>
+            <input type="text" placeholder="Search here" />
+        </div>
+      
+    </div>
+  );
+}
+
+export default Search;
